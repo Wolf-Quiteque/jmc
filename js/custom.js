0513d61c4bc64ec44eb16a59d2f5dcaf1c4046b0
Wolf-quiteque <marcioqui3@gmail.com>
2025-06-26 17:04:15 +0100

    lkjj

--- js/custom.js
+++ js/custom.js
@@ -689,3 +689,23 @@ $(function () {
               });
             }
           })();
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+		dots: false,
+		autoplay:true,
+		autoplayTimeout:5000,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 5
+            }
+        }
+    })
